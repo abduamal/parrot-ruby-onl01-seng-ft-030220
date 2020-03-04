27c327e1e6a17eab4efc6607a46ac882321e91cc
Amal Abdu <abduamal@ymail.com>
2020-03-04 01:25:28 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,10 +1,10 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(str)
-
+def parrot("Pretty Bird")
+  puts parrot
 end
 
-def parrot
-  puts "Squawk!"
+def parrot(phrase = "Squawk!")
+  puts phrase
 end
